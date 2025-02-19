3ba5444244fbbc4976a05d8fd4918c3844c24727
nam-nguyen-1993 <nam.nguyen@chicagobooth.edu>
2025-02-19 06:36:14 +0000

    Movie profile

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,4 +2,11 @@ class MoviesController < ApplicationController
   def list_movie
     render({ :template => "pages_templates/movie" })
   end
+
+  def profile_movie
+    movie_id = params.fetch("movie_id")
+    record = Movie.where({ :id => movie_id })
+    @the_movie = record.at(0)
+    render({ :template => "pages_templates/movie_profile"})
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   get("/directors/:director_id", { :controller => "directors", :action => "profile_director" })
 
   get("/movies", {:controller => "movies", :action => "list_movie"})
+  get("/movies/:movie_id", { :controller => "movies", :action => "profile_movie" })
+
   get("/actors", { :controller => "actors", :action => "list_actor" })
 end
